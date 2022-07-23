34ad6abf30e34c239ae86494b17a258edb3a3e77
nirikshan <nirikshanbhusal@gmail.com>
2022-07-23 22:00:03 +0545

    init commit

---
+++ ios/Pods/Target Support Files/Pods-khaltipayrn-khaltipayrnTests/Pods-khaltipayrn-khaltipayrnTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_khaltipayrn_khaltipayrnTests : NSObject
+@end
+@implementation PodsDummy_Pods_khaltipayrn_khaltipayrnTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-khaltipayrn/Pods-khaltipayrn-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_khaltipayrn : NSObject
+@end
+@implementation PodsDummy_Pods_khaltipayrn
+@end
